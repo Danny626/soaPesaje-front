4ebbfe92ae7b385f634a4966c119495175c87a86
Danny <live626@gmail.com>
2022-04-18 11:28:59 -0400

    envinronments folder removed

--- src/environments/environment.prod.ts
+++
@@ -1,9 +0,0 @@
-export const environment = {
-  production: true,
-  HOST: 'http://localhost:8080/SoaPesaje-backend',
-  PATH_ROOT: '/SoaPesaje-backend',
-  REINTENTOS: 3,
-  TOKEN_AUTH_USERNAME: 'soapesaje',
-  TOKEN_AUTH_PASSWORD: '${TOKEN_AUTH_PASSWORD}',
-  TOKEN_NAME: 'access_token'
-};
